20ddbe7c0fac63b8611b300be30edbac90726bc6
wendy <mizukilee@gmail.com>
2023-01-31 10:12:32 -0500

    commit tree 82d3d3526fd2ce135c22fe549359a1df2ae878d4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title) 
+        @title = title
+    end
 
+    def turn_page 
+         puts(@turn_page = 'Flipping the page...wow, you read fast!') 
+    end
+end
 
